1486319a907e118e1da77ee3d7ca52ee6e88286e
JJoe <48448023+JJoeDev@users.noreply.github.com>
2023-11-24 00:10:42 +0100

    Initial upload

---
+++ src/fileManager.zig
@@ -0,0 +1,5 @@
+const std = @import("std");
+
+pub fn testrun() void {
+    std.debug.print("Hello World!\n", .{});
+}
---
+++ src/main.zig
@@ -0,0 +1,7 @@
+const std = @import("std");
+
+const fm = @import("fileManager.zig");
+
+pub fn main() !void {
+    fm.testrun();
+}
