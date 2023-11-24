713d31b6085557cc9a2fe9c4fa03235066cd90be
jjoedev <jhknuds1@protonmail.com>
2023-11-24 14:17:32 +0100

    Working on making it nice to work with or something

--- src/fileManager.zig
+++ src/fileManager.zig
@@ -1,5 +1,25 @@
 const std = @import("std");
 
-pub fn testrun() void {
-    std.debug.print("Hello World!\n", .{});
+const fs = std.fs;
+
+pub fn ListCommands(writer: fs.File.Writer) !void { // getStdOut.writer() returns writer from fs
+    const cmd1 = "c: create new object\n"; // *const [21:0]u8 | \n is one char
+    const cmd2 = "d: delete object from inventory\n"; // *const [32:0]u8 | \n is one char
+    const cmd3 = "a: add one to object in inventory\n";
+    const cmd4 = "s: subtract one from object in inventory\n";
+
+    try writer.print("{s}{s}{s}{s}", .{ cmd1, cmd2, cmd3, cmd4 });
+}
+
+pub fn ShowInventory() !void {
+    const inventory = try fs.cwd().openFile("Inventory.txt", .{});
+    defer inventory.close();
+
+    var bufReader = std.io.bufferedReader(inventory.reader());
+    var inStream = bufReader.reader();
+
+    var buf: [1024]u8 = undefined;
+    while (try inStream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        std.debug.print("{s}\n", .{line});
+    }
 }
--- src/main.zig
+++ src/main.zig
@@ -3,5 +3,16 @@ const std = @import("std");
 const fm = @import("fileManager.zig");
 
 pub fn main() !void {
-    fm.testrun();
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
+
+    _ = stdin;
+
+    try stdout.print("Current inventory:\n\n", .{});
+
+    try fm.ShowInventory();
+
+    try stdout.print("\n\nSelect operation:\n", .{});
+
+    try fm.ListCommands(stdout);
 }
